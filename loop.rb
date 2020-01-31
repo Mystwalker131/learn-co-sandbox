6b7606b5a0fb790659a05e6a669b15cda702be08
Benito Galvez <mystwalker131@gmail.com>
2020-01-31 05:55:10 +0000

    Automatically backed up by Learn

---
+++ loop.rb
@@ -0,0 +1,13 @@
+ loop do
+   puts "Never pass a school bus if it has flashing red lights"
+ end
+
+
+# primary_colors = ["Red", "Yellow", "Blue"]
+# primary_colors.each do |color| # do begins a block
+#   # the lines between the do/end are the block's body
+#   puts "Primary Color #{color} is #{color.length} letters long."
+# end # end terminates the block
+
+
+
